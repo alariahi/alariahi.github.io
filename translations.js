2c09b29d2f10bbb5d5096beffe0627c9e97c2921
alaeddineriahi <alaeddine.riahi@esprit.com>
2024-03-21 04:25:32 +0100

    first deploy

---
+++ translations.js
@@ -0,0 +1,217 @@
+var translations = {
+  en: {
+    TolePerforrée:"Perforated Sheets 5/10 G 6/10 Small & Big hole 2000X10000",
+    "all, .Fer_Marchands, .Poutrelles, .Toles, .Inox, .Aluminium, .Panneaux_Sandwich, .Caillebotis, .Tubes, .Lames_Rideaux, .Ressort": "All",
+    Fer_Marchands: "Merchant Iron",
+    Poutrelles: "Beams",
+    Toles: "Sheets",
+    Inox: "Stainless Steel",
+    Aluminium: "Aluminum",
+    Panneaux_Sandwich: "Sandwich Panels",
+    Caillebotis: "Gratings",
+    Tubes: "Tubes",
+    Lames_Rideaux: "Curtain Blades",
+    Ressort: "Spring",
+    TubeCarréGalvanisée: "Galvanized Square Tube",
+    FerPlat: "Flat Iron",
+    ToleAluminium: "Aluminum Sheet",
+    TubeInox: "Stainless Steel Tube",
+    LameRideauxBombée: "Curved Curtain Blade",
+    "ToleInox3042B+PVC": "Stainless Steel Sheet 304 2B + PVC",
+    RessortàLames: "Leaf Spring",
+    PanneauxSandwich: "Sandwich Panels",
+    TolesPerforrées: "Perforated Sheets",
+    FerHea: "Iron Hea",
+    TolesFortes: "Strong Sheets",
+    CaillebotisGalva: "Galvanized Grating",
+    TubesRondsLAF: "LAF Round Tubes",
+    TubesRectangulairesGalvanisées: "Galvanized Rectangular Tubes",
+    TubesRondLAC: "LAC Round Tubes",
+    FerEnU: "U Iron",
+    Corniere: "Angle",
+    FerIPE: "IPE Iron",
+    FerHEB: "HEB Iron",
+    FerUPN: "UPN Iron",
+    TolesLaminéesàChaudLarmées: "Hot Rolled Checkered Sheets",
+    TolesLAF: "LAF Sheets",
+    TolesNervuréesGalvanisées: "Galvanized Ribbed Sheets",
+    TolesLAC: "LAC Sheets",
+    TolesGalvanisées: "Galvanized Sheets",
+    TolesMicroNervurées: "Micro Ribbed Sheets",
+    TolesOndulées: "Corrugated Sheets",
+    TolesStriées: "Striated Sheets",
+    "TolesAluminiumD.D": "Ribbed Aluminium Sheets",
+    TolesAluminiumStriées: "Ribbed Aluminium Sheets",
+    TubesCarrésLAC: "LAC Square Tubes",
+    TubesCarrésLAF: "LAF Square Tubes",
+    TubesRectangulairesLAC: "LAC Rectangular Tubes",
+    FerRond: "Round Iron",
+    FerCarré: "Square Iron",
+    FerenT: "T Iron",
+    TubesRondLAC: "LAC Round Tubes",
+    'bannerWelcome': 'Welcome to',
+    'bannerText': 'Explore our select range of metal and steel products, perfect for construction and industrial use: merchant bars, stainless steel, aluminum, galvanized sheets, pipes, and beams. Quality materials for your every need.',
+    'bannerBtn': 'Check our Products',
+    aboutSubTitle: "About Us",
+    aboutTitle: "Discover our Company story",
+    aboutSpan: "Company story",
+    aboutDesc: "step into the world of Prometal PM, your trusted partner in metal and steel solutions since 1990. Nestled in the heart of Ben Arous region’s Industrial Zone Megrine Saint Gobain, we are not just a company; we are a legacy of quality and trust. Specializing in the importation and commercialization of a premium selection of metal and steel products, we are dedicated to building the future with you. Discover the strength and durability that has been our promise for over three decades. Welcome to Prometal PM – where metal meets excellence.",
+    aboutWatch: "Watch",
+    scheduleWeekdays: "Monday to Thursday",
+    scheduleWeekdaysTime1: "08:00 am - 13:00 pm",
+    scheduleWeekdaysTime2: "14:00 am - 17:00 pm",
+    scheduleFriday: "Friday",
+    scheduleFridayTime1: "08:00 am - 12:30 pm",
+    scheduleFridayTime2: "14:00 am - 17:00 pm",
+    scheduleSaturday: "Saturday",
+    scheduleSaturdayTime: "08:00 am to 13:00 pm",
+    contactSubtitle: "Contact Us",
+    contactTitle: "Contact & Opening Hours",
+    footerQuality: "Quality materials for your every need.",
+footerContactUs: "Contact Us",
+footerLinks: "Links",
+footerHome: "Home",
+footerAbout: "About",
+footerMenu: "Menu",
+footerGallery: "Gallery",
+footerBlog: "Blog",
+footerContact: "Contact",
+footerFindUs: "Where To Find Us",
+"home": "Home",
+        "about": "About",
+        "menu": "Products",
+        "gallery": "Gallery",
+        "blog": "Blog",
+        "contact": "Contact"
+
+  },
+  fr: {
+    TolePerforrée:"TÔLE PERFOREE 5/10 G 6/10 PETIT & GRAND TROU 2000X10000",
+    "all, .Fer_Marchands, .Poutrelles, .Toles, .Inox, .Aluminium, .Panneaux_Sandwich, .Caillebotis, .Tubes, .Lames_Rideaux, .Ressort": "Tout",
+    Fer_Marchands: "Fer Marchands",
+    Poutrelles: "Poutrelles",
+    Toles: "Tôles",
+    Inox: "Inox",
+    Aluminium: "Aluminium",
+    Panneaux_Sandwich: "Panneaux Sandwich",
+    Caillebotis: "Caillebotis",
+    Tubes: "Tubes",
+    Lames_Rideaux: "Lames de Rideaux",
+    Ressort: "Ressort",
+    TubeCarréGalvanisée: "Tube Carré Galvanisé",
+    FerPlat: "Fer Plat",
+    ToleAluminium: "Tôle Aluminium",
+    TubeInox: "Tube Inox",
+    LameRideauxBombée: "Lame Rideaux Bombée",
+    "ToleInox3042B+PVC": "Tôle Inox 304 2B + PVC",
+    RessortàLames: "Ressort à Lames",
+    PanneauxSandwich: "Panneaux Sandwich",
+    TolesPerforrées: "Tôles Perforées",
+    FerHea: "Fer Hea",
+    TolesFortes: "Tôles Fortes",
+    CaillebotisGalva: "Caillebotis Galvanisé",
+    TubesRondsLAF: "Tubes Ronds LAF",
+    TubesRectangulairesGalvanisées: "Tubes Rectangulaires Galvanisés",
+    TubesRondLAC: "Tubes Rond LAC",
+    FerEnU: "Fer En U",
+    Corniere: "Cornière",
+    FerIPE: "Fer IPE",
+    FerHEB: "Fer HEB",
+    FerUPN: "Fer UPN",
+    TolesLaminéesàChaudLarmées: "Tôles Laminées à Chaud Larmées",
+    TolesLAF: "Tôles LAF",
+    TolesNervuréesGalvanisées: "Tôles Nervurées Galvanisées",
+    TolesLAC: "Tôles LAC",
+    TolesGalvanisées: "Tôles Galvanisées",
+    TolesMicroNervurées: "Tôles Micro Nervurées",
+    TolesOndulées: "Tôles Ondulées",
+    TolesStriées: "Tôles Striées",
+    "TolesAluminiumD.D": "Tôles Aluminium D.D",
+    TolesAluminiumStriées: "Tôles Aluminium Striées",
+    TubesCarrésLAC: "Tubes Carrés LAC",
+    TubesCarrésLAF: "Tubes Carrés LAF",
+    TubesRectangulairesLAC: "Tubes Rectangulaires LAC",
+    FerRond: "Fer Rond",
+    FerCarré: "Fer Carré",
+    FerenT: "Fer en T",
+    TubesRondLAC: "Tubes Rond LAC",
+    'bannerWelcome': 'Bienvenue chez ',
+    'bannerText': 'Découvrez notre gamme sélectionnée de produits en métal et en acier, parfaite pour la construction et l\'utilisation industrielle : barres marchandes, acier inoxydable, aluminium, tôles galvanisées, tubes et poutres. Des matériaux de qualité pour tous vos besoins.',
+    'bannerBtn': 'Vérifiez nos produits',
+    aboutSubTitle: "À propos de nous",
+    aboutTitle: "Découvrez notre histoire d'entreprise",
+    aboutSpan: "histoire d'entreprise",
+    aboutDesc: "Entrez dans le monde de Prometal PM, votre partenaire de confiance pour les solutions en métal et en acier depuis 1990. Situé au cœur de la Zone Industrielle de Megrine Saint Gobain dans la région de Ben Arous, nous ne sommes pas seulement une entreprise ; nous sommes un héritage de qualité et de confiance. Spécialisés dans l'importation et la commercialisation d'une sélection premium de produits en métal et en acier, nous sommes dédiés à construire l'avenir avec vous. Découvrez la force et la durabilité qui ont été notre promesse pendant plus de trois décennies. Bienvenue chez Prometal PM – là où le métal rencontre l'excellence.",
+    aboutWatch: "Regarder",
+    scheduleWeekdays: "Du lundi au jeudi",
+    scheduleWeekdaysTime1: "08h00 - 13h00",
+    scheduleWeekdaysTime2: "14h00 - 17h00",
+    scheduleFriday: "Vendredi",
+    scheduleFridayTime1: "08h00 - 12h30",
+    scheduleFridayTime2: "14h00 - 17h00",
+    scheduleSaturday: "Samedi",
+    scheduleSaturdayTime: "08h00 à 13h00",
+    contactSubtitle: "Contactez-nous",
+    contactTitle: "Contact et horaires d'ouverture",
+    footerQuality: "Matériaux de qualité pour tous vos besoins.",
+    footerContactUs: "Contactez-nous",
+    footerLinks: "Liens",
+    footerHome: "Accueil",
+    footerAbout: "À propos",
+    footerMenu: "Menu",
+    footerGallery: "Galerie",
+    footerBlog: "Blog",
+    footerContact: "Contact",
+    footerFindUs: "Où nous trouver",
+    "home": "Accueil",
+    "about": "À propos",
+    "menu": "Produits",
+    "gallery": "Galerie",
+    "blog": "Blog",
+    "contact": "Contact"
+  },
+};
+var defaultLanguage = "fr";
+
+function switchLanguage(language) {
+    // Find all elements that have a 'data-filter' attribute
+    var elements = document.querySelectorAll("[data-filter]");
+  
+    // Update each element with the new language
+    elements.forEach(function(element) {
+      var filter = element.getAttribute("data-filter").replace(".", "");
+  
+      // Check if translation exists for the language and filter key
+      if (translations[language] && translations[language][filter]) {
+        // Remove existing text nodes to prevent duplication
+        Array.from(element.childNodes).forEach(function(node) {
+          if (node.nodeType === Node.TEXT_NODE) {
+            element.removeChild(node);
+          }
+        });
+  
+        // Add new text node with the translation
+        var newText = document.createTextNode(translations[language][filter]);
+        element.appendChild(newText);
+      } else {
+        console.error("Translation not found for: " + filter);
+      }
+    });
+  }
+  
+  function setDefaultLanguage() {
+    switchLanguage(defaultLanguage);
+}
+  
+setDefaultLanguage();  
+  
+
+document.getElementById("switch-to-fr").addEventListener("click", function() {
+    switchLanguage("fr");
+});
+
+document.getElementById("switch-to-en").addEventListener("click", function() {
+    switchLanguage("en");
+});
+
+  
